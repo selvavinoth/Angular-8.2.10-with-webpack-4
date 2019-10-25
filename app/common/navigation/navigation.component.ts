f29d6d6bfed9578940bf05801ac1f4b9283b536b
Selva Vinoth <Selva.Vinoth@cygate.se>
2019-10-25 16:35:05 +0530

    Navigation_Init

--- app/common/navigation/navigation.component.ts
+++ app/common/navigation/navigation.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'navigation',
-    templateUrl: 'navigation.component.html'
+    templateUrl: 'navigation.component.html',
+    styleUrls: ['./navigation.component.scss']
 })
 
 export class NavigationComponent implements OnInit {
